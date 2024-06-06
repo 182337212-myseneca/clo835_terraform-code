b3376abc72fbd753d19e5b35ca411e1b03220bdf
182337212-myseneca <153153154+182337212-myseneca@users.noreply.github.com>
2024-06-06 11:17:18 -0400

    Update main.tf

--- EC2_instance/main.tf
+++ EC2_instance/main.tf
@@ -30,9 +30,10 @@ data "aws_ami" "latest_amazon_linux" {
   }
 }
 
-# Use existing key pair
-data "aws_key_pair" "existing_key" {
-  key_name = "asskey1"  # Replace with the name of your existing key pair
+# Create a new key pair
+resource "aws_key_pair" "web_key" {
+  key_name   = "${var.prefix}-key"
+  public_key = file("${var.prefix}.pub")
 }
 
 # Use existing security group
@@ -49,7 +50,7 @@ data "aws_security_group" "existing_sg" {
 resource "aws_instance" "ec2_instances_webservers" {
   ami                         = data.aws_ami.latest_amazon_linux.id
   instance_type               = var.instance_type
-  key_name                    = data.aws_key_pair.existing_key.key_name
+  key_name                    = aws_key_pair.web_key.key_name
   subnet_id                   = data.aws_subnets.subnets_public.ids[0]
   security_groups             = [data.aws_security_group.existing_sg.id]
   associate_public_ip_address = true
@@ -59,56 +60,6 @@ resource "aws_instance" "ec2_instances_webservers" {
     "Name" = "${var.prefix}-WebServer"
   })
 }
-
-# Commented out as we are using existing key pair
-# resource "aws_key_pair" "web_key" {
-#   key_name   = "${var.prefix}"
-#   public_key = file("${var.prefix}.pub")
-# }
-
-# Commented out as we are using existing security group
-# resource "aws_security_group" "web_sg" {
-#   name        = "allow_http_ssh"
-#   description = "Allow HTTP and SSH inbound traffic"
-#   vpc_id      = data.aws_vpc.vpc.id
-#   
-#   ingress {
-#     description      = "Allow HTTP Connection"
-#     from_port        = 80
-#     to_port          = 80
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
-# 
-#   ingress {
-#     description      = "Allow SSH Connection"
-#     from_port        = 22
-#     to_port          = 22
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
-#   
-#   ingress {
-#     description      = "Allow Custom TCP for ports 8081, 8082, 8083 From Everywhere"
-#     from_port        = 8081
-#     to_port          = 8083
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
-# 
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   
-#   }
-#   
-#   tags = merge(var.default_tags, {
-#     "Name" = "${var.prefix}-SG-webserver"
-#     }
-#   )
-# }
 
 resource "aws_ecr_repository" "ecr-app" {
   name                 = "assignment-1-app"
@@ -124,6 +75,6 @@ resource "aws_ecr_repository" "ecr-db" {
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = trueclean
   }
 }
